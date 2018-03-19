c9508883df814642ffd2eb6a6f4dd1b7a1da8463
Mahmudul-EIS <35486228+Mahmudul-EIS@users.noreply.github.com>
2018-03-19 12:13:19 +0600

    Update guide-login-header.blade.php

--- resources/views/includes/guide-login-header.blade.php
+++ resources/views/includes/guide-login-header.blade.php
@@ -9,6 +9,7 @@
                     <div class="social-media">
                         <ul>
                             <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" class="shopping-cart-popup"><i class="fa fa-shopping-cart"></i></a></li>  
                         </ul>
                     </div>
                 </div>
@@ -34,6 +35,7 @@
                 <ul>
                     <li><a href="{{ url('/') }}"><i class="fa fa-facebook"></i></a></li>
                     <li><a class="open-popup" href="#"><i class="fa fa-search"></i></a></li>
+                     <li><a href="#" class="shopping-cart-popup"><i class="fa fa-shopping-cart"></i></a></li>
                 </ul>
             </div>
 
