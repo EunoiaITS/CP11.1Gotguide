30789586b5c3fc2476c647add09447fc08036cc4
Mahmudul-EIS <35486228+Mahmudul-EIS@users.noreply.github.com>
2018-03-19 14:14:33 +0600

    Update guide-profile.blade.php
    I add shopping card and logout confirmation popup modal in this page

--- resources/views/pages/guide-profile.blade.php
+++ resources/views/pages/guide-profile.blade.php
@@ -2248,6 +2248,34 @@
 
             </div>
         </div><!-- Popup -->
+
+         <!--=======================
+        log out confirmation
+        ==========================-->
+         <div class="log-out-confermation-popup">
+            <div class="popup-base">
+                <div class="popup-rateme-main">
+                    <i class="close fa fa-remove"></i>
+                    <div class="row">
+                        <div class="rateme-destination">
+                            <h2 class="search-title">Confirmation Logout</h2>
+                        </div>
+                        <!-- header got seach area -->
+                        <div class="popup-got-search button-option-logout">
+                            <form action="#">
+                               <p class="text-center">Are you sure you want to logout ?</p>
+                               <div class="new-button clearfix">
+                                   <button class="btn btn-info btn-change">Ok</button>
+                                   <button class="btn btn-info btn-change close">Cancel</button>
+                               </div>
+                            </form>
+                        </div><!--// end header got search area -->
+                    </div>
+                </div>
+
+            </div>
+        </div><!-- Popup -->
+
 <script type="text/javascript">
     function sortCity(){
         var sel = document.getElementById('city');
