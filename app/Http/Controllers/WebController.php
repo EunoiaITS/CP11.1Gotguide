bf5b7ba2b25524976d24a2a4ad2a6ccff7554746
Mashuk-EIS <mushfiqur.rahman@eunoiaits.com>
2018-03-21 11:34:52 +0600

    Changes

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -195,20 +195,20 @@ class WebController extends Controller
             '<h1>Hi '.$request->fname.' '.$request->lname.',</h1>'.
             '<p style="font-size:18px;">Your account registration is complete. Please click the button/link below to verify your account.</p>'.
             '<table width="100%" border="0" cellspacing="0" cellpadding="0">
-                          <tr>
-                            <td>
-                              <div>
-                                <!--[if mso]>
-                                  <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://litmus.com" style="height:36px;v-text-anchor:middle;width:150px;" arcsize="5%" strokecolor="#EB7035" fillcolor="#EB7035">
-                                    <w:anchorlock/>
-                                    <center style="color:#ffffff;font-family:Helvetica, Arial,sans-serif;font-size:16px;">I am a button &rarr;</center>
-                                  </v:roundrect>
-                                <![endif]-->
-                                <a href="'.$link.'" style="background-color:#EB7035;border:1px solid #EB7035;border-radius:3px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:16px;line-height:44px;text-align:center;text-decoration:none;width:150px;-webkit-text-size-adjust:none;mso-hide:all;">Verify Account &rarr;</a>
-                              </div>
-                            </td>
-                          </tr>
-                        </table>'.
+                  <tr>
+                      <td>
+                          <div>
+                          <!--[if mso]>
+                              <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://litmus.com" style="height:36px;v-text-anchor:middle;width:150px;" arcsize="5%" strokecolor="#EB7035" fillcolor="#EB7035">
+                              <w:anchorlock/>
+                              <center style="color:#ffffff;font-family:Helvetica, Arial,sans-serif;font-size:16px;">I am a button &rarr;</center>
+                              </v:roundrect>
+                              <![endif]-->
+                          <a href="'.$link.'" style="background-color:#EB7035;border:1px solid #EB7035;border-radius:3px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:16px;line-height:44px;text-align:center;text-decoration:none;width:150px;-webkit-text-size-adjust:none;mso-hide:all;">Verify Account &rarr;</a>
+                          </div>
+                      </td>
+                  </tr>
+             </table>'.
             '<br><br>Thank You<br>Got Guide<br>Customer Care Team</body></html>',
             'text/html');
 
